2ff4388c7b03a6c68e09d0a017855943668e483b
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-16 02:17:56 +0200

    Add initial

---
+++ Pubc/Version.cpp
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#include "IrisWidget/Pubc/Version.h"
+
+#include "IrisWidget/-genc/def_repo_version.h"
+#include "IrisWidget/-genc/def_contribute.h"
+
+namespace IrisWidget {
+	namespace Core {
+		
+		BR_VERSION_DEFINE(IrisWidget);
+        BR_CONTRIBUTE_DEFINE(IrisWidget);
+        BR_PROJECT_DEFINE(IrisWidget);
+
+	}
+} 
